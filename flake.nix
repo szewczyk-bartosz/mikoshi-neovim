8b7507d8a622d99102d46bfb7dc5a393b6410e6a
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-01-10 18:57:26 +0000

    added support for python and nix

--- flake.nix
+++ flake.nix
@@ -1,56 +1,118 @@
 {
-	inputs = {
-		nixpkgs.url = "nixpkgs/nixos-unstable";
-		
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
 
-		nvf = {
-			url = "github:NotAShelf/nvf";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	};
+  outputs = {
+    self,
+    nixpkgs,
+    nvf,
+  }: {
+    nixosModules.default = {
+      config,
+      pkgs,
+      lib,
+      ...
+    }: {
+      imports = [nvf.nixosModules.default];
+      programs.nvf = {
+        enable = true;
+        settings.vim = {
+          vimAlias = false; # for now
 
-	outputs = { self, nixpkgs, nvf }: 
-	{
-		nixosModules.default = { config, pkgs, lib, ... }: {
-			imports = [ nvf.nixosModules.default ];
-			programs.nvf = {
-				enable = true;
-				settings.vim = {
-					vimAlias = false; # for now
+          lineNumberMode = "number";
 
-					lineNumberMode = "number";
+          clipboard = {
+            enable = true;
+            registers = "unnamedplus";
+            providers.wl-copy.enable = true;
+          };
 
-					clipboard.enable = true;
-					clipboard.registers = "unnamedplus";
+          options = {
+            tabstop = 4;
+            shiftwidth = 4;
+            expandtab = true;
+          };
 
-					options = {
-						tabstop = 4;
-						shiftwidth = 4;
-						expandtab = true;
-					};
+          keymaps = [
+            {
+              key = "jk";
+              mode = "i";
+              action = "<Esc>";
+            }
+          ];
 
-					autocmds = [
-						{
-							desc = "set tabstop and tabwidth to 2 for nix files";
-							event = ["FileType"];
-							pattern = ["nix"];
-							callback = lib.generators.mkLuaInline ''
-							function()
-									vim.opt.tabstop = 2
-									vim.opt.shiftwidth = 2
-							end
-							'';
-						}
-					];
+          lsp = {
+            enable = true;
+            formatOnSave = true;
+          };
 
-					theme = {
-						enable = true;
-						name = "tokyonight";
-						style = "night";
-					};
-				};
-			};
-		};
-	};
+          languages = {
+            nix = {
+              enable = true;
+              format.enable = true;
+              lsp.enable = true;
+            };
+
+            python = {
+              enable = true;
+              format.enable = true;
+              lsp.enable = true;
+            };
+          };
+
+          treesitter = {
+            enable = true;
+            grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+              c
+              python
+              rust
+              java
+              javascript
+              html
+              css
+              nix
+            ];
+          };
+
+          autocmds = [
+            {
+              desc = "set tabstop and tabwidth to 2 for nix files";
+              event = ["FileType"];
+              pattern = ["nix"];
+              callback = lib.generators.mkLuaInline ''
+                function()
+                		vim.opt.tabstop = 2
+                		vim.opt.shiftwidth = 2
+                end
+              '';
+            }
+            {
+              desc = "disable indentation for nix files (the indenter is broken)";
+              event = ["FileType"];
+              pattern = ["nix"];
+              callback = lib.generators.mkLuaInline ''
+                              function()
+                                vim.opt.indentexpr = ""
+                                vim.opt.autoindent = true
+                                vim.opt.smartindent = true
+                end
+              '';
+            }
+          ];
+
+          theme = {
+            enable = true;
+            name = "tokyonight";
+            style = "night";
+          };
+        };
+      };
+    };
+  };
 }
